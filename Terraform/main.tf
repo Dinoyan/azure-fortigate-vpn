673a54e13bc15937e6139ac50eedbbb5eb6e2504
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2021-04-01 15:22:23 -0400

    Gateway subnet + public ip

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,3 +27,19 @@ resource "azurerm_virtual_network" "vnet" {
 	address_prefix = "10.10.0.0/27"
 	}
 }
+
+resource "azurerm_subnet" "gwsubnet" {
+	name = "GatewaySubnet"
+	resource_group_name = azurerm_resource_group.rg.name
+	virtual_network_name = azurerm_virtual_network.vnet.name
+	address_prefixes = ["10.10.0.32/28"]
+}
+
+resource "azurerm_public_ip" "pubip" {
+	name = "gatway-pub-ip"
+	location = azurerm_resource_group.rg.location
+	resource_group_name = azurerm_resource_group.rg.name
+
+	allocation_method = "Dynamic"
+}
+
