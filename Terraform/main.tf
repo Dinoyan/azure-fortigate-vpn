f61028880a8fff8e41b7427f88624f7affd993ad
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2021-04-02 13:26:10 -0400

    Virtual Network Gateway creation

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,3 +43,23 @@ resource "azurerm_public_ip" "pubip" {
 	allocation_method = "Dynamic"
 }
 
+
+resource "azurerm_virtual_network_gateway" "vng" {
+	name = "vpn-gateway"
+	location = azurerm_resource_group.rg.location
+	resource_group_name = azurerm_resource_group.rg.name
+
+	type = "Vpn"
+	vpn_type = "PolicyBased"
+
+	active_active = false
+	enable_bgp = false
+	sku = "Basic"
+
+	ip_configuration {
+		name = "vnetGWip"
+		public_ip_address_id = azurerm_public_ip.pubip.id
+		private_ip_address_allocation = "Dynamic"
+		subnet_id = azurerm_subnet.gwsubnet.id
+	}
+}
